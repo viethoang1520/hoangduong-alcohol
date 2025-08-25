33dc87ce91ba374a3616532a417d2b50521a5b16
viethoang1520 <viethoang1520.dev@gmail.com>
2025-08-25 19:42:57 +0700

    fix social button

--- src/pages/Login/SocialButton/SocialButton.jsx
+++ src/pages/Login/SocialButton/SocialButton.jsx
@@ -5,7 +5,7 @@ function SocialButton({label, icon, type}) {
   return (
     <div className='social-button-wrapper'>
       <div className="icon-wrapper">
-        <Icon icon={icon} width="22" height="22" />
+        <Icon icon={icon} width="18" height="18" />
       </div>
       <span className='social-button-label'>{label}</span>
     </div>
