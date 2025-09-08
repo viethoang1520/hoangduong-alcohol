be5018cc8ffeb6c047807ce51f8966b9800f744a
viethoang1520 <viethoang1520.dev@gmail.com>
2025-09-08 15:18:36 +0700

    fix: ui wine detail page

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -10,17 +10,17 @@ export default function ProductDetails() {
   const { id } = useParams();
   const location = useLocation();
   const [wine, setWine] = useState(null);
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
   const fetchWine = async () => {
     try {
-      setLoading(true); 
+      setLoading(true);
       const res = await getWineById(id);
       setWine(res);
     } catch (error) {
       console.log(error);
     } finally {
-      setLoading(false); 
+      setLoading(false);
     }
   };
 
@@ -42,40 +42,43 @@ export default function ProductDetails() {
   }
 
   return (
-    <div className="container">
+    <div>
       <NavigateLine />
-      <Row gutter={32} className="product-details-block">
-        <Col xl={15} lg={12} md={12} sm={24} xs={24}>
-          <img className="product-image" src={wine?.image} alt="" />
-        </Col>
-        <Col className="product-detail-wrapper" xl={9} lg={12} md={12} sm={24} xs={24}>
-          <h1 className="product-detail-name">{wine?.name}</h1>
-          <p className="product-detail-identifier">SKU:RCV18M </p>
-          <div className="price-line"></div>
-          <h2 className="product-detail-price">{(wine?.price * 26000).toLocaleString()}₫</h2>
-          <div className="price-line"></div>
-          <div className="product-quantity">
-            <label className="product-detail-info-label">Số lượng: </label>
-            <InputNumber min={1} max={10} defaultValue={1} onChange={() => { }} changeOnWheel />
-          </div>
-          <p className="note product-detail-info-label"> Ghi chú</p>
-          <Input placeholder="Thêm ghi chú" />
-          <Button
-            width={370}
-            height={50}
-            content={'Mua ngay'}
-            buttonType={'primary'}
-          />
-          <h2 className="product-detail-label">Chi tiết sản phẩm </h2>
-          <h1 className="product-detail-info-label">Thông tin sản phẩm</h1>
-          <p className="product-detail-info-label">- Xuất xứ: </p>
-          <p className="product-detail-info-label">- Thương hiệu: {wine?.brand}</p>
-          <p className="product-detail-info-label">- Tuổi rượu: {wine?.vintage}</p>
-          <p className="product-detail-info-label">- Nồng Độ: {wine?.alcoholContent}</p>
-          <p className="product-detail-info-label">- Dung Tích: {wine?.volume}ml </p>
-          <p className="product-detail-info-label">- Available: {wine?.stockQuantity} bottle(s) </p>
-        </Col>
-      </Row>
+      <div className="container">
+
+        <Row gutter={32} className="product-details-block">
+          <Col xl={15} lg={12} md={12} sm={24} xs={24}>
+            <img className="product-image" src={wine?.image} alt="" />
+          </Col>
+          <Col className="product-detail-wrapper" xl={9} lg={12} md={12} sm={24} xs={24}>
+            <h1 className="product-detail-name">{wine?.name}</h1>
+            <p className="product-detail-identifier">SKU:RCV18M </p>
+            <div className="price-line"></div>
+            <h2 className="product-detail-price">{(wine?.price * 26000).toLocaleString()}₫</h2>
+            <div className="price-line"></div>
+            <div className="product-quantity">
+              <label className="product-detail-info-label">Số lượng: </label>
+              <InputNumber min={1} max={10} defaultValue={1} onChange={() => { }} changeOnWheel />
+            </div>
+            <p className="note product-detail-info-label"> Ghi chú</p>
+            <Input placeholder="Thêm ghi chú" />
+            <Button
+              width={370}
+              height={50}
+              content={'Mua ngay'}
+              buttonType={'primary'}
+            />
+            <h2 className="product-detail-label">Chi tiết sản phẩm </h2>
+            <h1 className="product-detail-info-label">Thông tin sản phẩm</h1>
+            <p className="product-detail-info-label">- Xuất xứ: </p>
+            <p className="product-detail-info-label">- Thương hiệu: {wine?.brand}</p>
+            <p className="product-detail-info-label">- Tuổi rượu: {wine?.vintage}</p>
+            <p className="product-detail-info-label">- Nồng Độ: {wine?.alcoholContent}</p>
+            <p className="product-detail-info-label">- Dung Tích: {wine?.volume}ml </p>
+            <p className="product-detail-info-label">- Available: {wine?.stockQuantity} bottle(s) </p>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
